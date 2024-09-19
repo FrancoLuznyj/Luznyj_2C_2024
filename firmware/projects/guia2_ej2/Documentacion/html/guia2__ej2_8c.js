07e82a42e084fab2a026e4bd365847e5717e9ae7
Luznyj <francoluznyj1999@gmail.com>
2024-09-19 20:55:26 -0300

    Se sube resolucion de ejercicio 2 de guia 2 con documentacion.

---
+++ firmware/projects/guia2_ej2/Documentacion/html/guia2__ej2_8c.js
@@ -0,0 +1,15 @@
+var guia2__ej2_8c =
+[
+    [ "CONFIG_BLINK_PERIOD_MEDIR_US", "guia2__ej2_8c.html#a389b03d9b07a859d2dbc7c2f10cc1afc", null ],
+    [ "CONFIG_BLINK_PERIOD_MOSTRAR_US", "guia2__ej2_8c.html#a35c445924a01160f8608adc1f2e91785", null ],
+    [ "app_main", "guia2__ej2_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "MedirTimer", "guia2__ej2_8c.html#a1806deede8a631dadbef9c6d8f34a3c3", null ],
+    [ "MostrarTimer", "guia2__ej2_8c.html#a47236e99abe4164d693f8a7f6539b837", null ],
+    [ "Tecla1", "guia2__ej2_8c.html#ae7b2398b45b3a4e147bb773f6d7fd915", null ],
+    [ "Tecla2", "guia2__ej2_8c.html#aab4aa84a51d1ae0bc3198a14fd573653", null ],
+    [ "distancia", "guia2__ej2_8c.html#aec40b15ff3098bdb2425eb4dc9c11a18", null ],
+    [ "encendido", "guia2__ej2_8c.html#a5332d02d82dacdfb5c6dc01cb428b013", null ],
+    [ "hold", "guia2__ej2_8c.html#af0129e079b57e897edf00f22cc25f3d4", null ],
+    [ "MedirDistanciaTasks_handle", "guia2__ej2_8c.html#af870da9176ac1b009da857a1122125c6", null ],
+    [ "MostrarDistanciaTasks_handle", "guia2__ej2_8c.html#a5c6beb1f7573e1a5a9fbeee126e09fcd", null ]
+];
--- firmware/projects/guia2_ej2/main/guia2_ej2.c
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -2,7 +2,12 @@
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
+ * En el siguiente ejercicio se desarrolla un firmware para la lectura de distancia con el sensor HC-SR04. La medicion
+ * se inicia o se apaga pulsando el switch 1. Un vez medida la distancia, se enciende uno o mas LEDs dependiendo del
+ * valor medido y se muestra la medicion en el display LCD. Al presionarse el pulsador switch 2 permite retener el valor
+ * medido en el display.
+ * El firmware emplea interrupciones para manejar el uso de los switches y temporizadores para establecer cada
+ * cuanto tiempo se realizan las tareas.
  *
  * <a href="https://drive.google.com/...">Operation Example</a>
  *
@@ -10,14 +15,17 @@
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | 	ECHO    	| 	GPIO_3	    |
+ * | 	TRIGGER   	| 	GPIO_2	    |
+ * | 	+5V 	 	| 	+5V 		|
+ * | 	GND 	 	| 	GND 		|
  *
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 09/09/2024 | Document creation		                         |
+ * | 18/09/2024 | Document creation		                         |
  *
  * @author Franco Luznyj (francoluznyj1999@gmail.com)
  *
@@ -39,28 +47,84 @@
 #define CONFIG_BLINK_PERIOD_MEDIR_US 1000*1000
 #define CONFIG_BLINK_PERIOD_MOSTRAR_US 1000*1000
 /*==================[internal data definition]===============================*/
+
+/// @brief Variable global de tipo booleana que se utiliza para establecer si se debe medir o si no se debe medir.
 bool encendido = false;
+
+/// @brief Variable global de tipo booleana que se utiliza para establecer si se mantiene la lectura en el display o no.
 bool hold = false;
+
+/// @brief Variable global de tipo entera sin signo de 16 bits que se utiliza para almacenar la medicion de la distancia.
 uint16_t distancia = 0;
+
+/// @brief Variable de tipo TaskHandle_t que almacena la referencia de la tarea que se encarga de medir la distancia.
 TaskHandle_t MedirDistanciaTasks_handle = NULL;
+
+/// @brief Variable de tipo TaskHandle_t que almacena la referencia de la tarea que se encarga de mostrar la distancia.
 TaskHandle_t MostrarDistanciaTasks_handle = NULL;
 /*==================[internal functions declaration]=========================*/
+
+/**
+ * @brief Funcion que se llama cuando se presiona el switch 1. Alterna el valor de la variable global encendido.
+ * 
+ * @param pvParameter Parametro no utilizado.
+ * 
+ * @return N/A
+ */
 void Tecla1(void *pvParameter){		
 	encendido = !encendido;
 }
 
+/**
+ * @brief Funcion que se llama cuando se presiona el switch 2. Alterna el valor de la variable global hold.
+ * 
+ * @param pvParameter Parametro no utilizado.
+ * 
+ * @return N/A
+ */
 void Tecla2(void *pvParameter){
 	hold = !hold;
 }
 
+/**
+ * @brief Funcion que se llama cuando finaliza el conteo del timer de medicion de distancia.
+ * 
+ * Esta funcion se encarga de notificar a la tarea MedirDistanciaTasks 
+ * para que vuelva a ejecutarse.
+ * 
+ * @param pvParameter Parametro no utilizado.
+ * 
+ * @return N/A
+ */
 void MedirTimer(void *pvParameter){
 	vTaskNotifyGiveFromISR(MedirDistanciaTasks_handle, pdFALSE);
 }
 
+/**
+ * @brief Funcion que se llama cuando finaliza el conteo del timer de mostrar distancia.
+ * 
+ * Esta funcion se encarga de notificar a la tarea MostrarDistanciaTasks 
+ * para que vuelva a ejecutarse.
+ * 
+ * @param pvParameter Parametro no utilizado.
+ * 
+ * @return N/A
+ */
 void MostrarTimer(void *pvParameter){
 	vTaskNotifyGiveFromISR(MostrarDistanciaTasks_handle, pdFALSE);
 }
 
+/**
+ * @brief Tarea que se encarga de medir la distancia con el sensor HC-SR04
+ * 
+ * La tarea se encarga de medir la distancia en centimetros y guardar el valor
+ * en la variable global distancia. La tarea se detiene si la variable encendido
+ * es false.
+ * 
+ * @param pvParameter El parametro para la tarea no es utilizado en este caso
+ * 
+ * @return N/A
+ */
 static void MedirDistanciaTasks(void *pvParameter){
 
 	while(true){
@@ -73,6 +137,24 @@ static void MedirDistanciaTasks(void *pvParameter){
 	}
 }
 
+/**
+ * @brief Tarea que se encarga de mostrar la distancia en centimetros en los LEDs
+ *       y en el display LCD.
+ * 
+ * La tarea se encarga de mostrar la distancia en centimetros en los LEDs
+ * segun la siguiente logica:
+ *   - si la distancia es menor a 10, apaga todos los LEDs.
+ *   - si la distancia esta entre 10 (inclusive) y 20, enciende el LED_1.
+ *   - si la distancia esta entre 20 (inclusive) y 30, enciende el LED_1 y el LED_2.
+ *   - si la distancia es mayor o igual a 30, enciende los 3 LEDs.
+ * La tarea tambien muestra la distancia en el display LCD, siempre y cuando
+ * la variable hold sea false, si es true se mantiene el valor de la mediciona previa.
+ * La tarea se detiene si la variable encendido es false.
+ * 
+ * @param pvParameter El parametro para la tarea no es utilizado en este caso
+ * 
+ * @return N/A
+ */
 static void MostrarDistanciaTasks(void *pvParameter){
 	while(true)
 	{
@@ -124,8 +206,8 @@ void app_main(void){
 	SwitchActivInt(SWITCH_1,Tecla1,NULL);
 	SwitchActivInt(SWITCH_2,Tecla2,NULL);
 
-	    /* Inicialización de timers */
-    timer_config_t timer_medir = {
+	    /* Inicialización de timers */ 
+	timer_config_t timer_medir = {
         .timer = TIMER_A,
         .period = CONFIG_BLINK_PERIOD_MEDIR_US,
         .func_p = MedirTimer,
